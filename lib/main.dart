ffbfd11a2184033419b430f64c5aae913cd5c73f
chera-mihiretu <cheramihiretucm@gmail.com>
2024-08-07 04:12:28 -0400

    Created data checker

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main (){
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,18 +15,44 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatefulWidget{
+class Home extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _Home();
   }
-
 }
 
 class _Home extends State<Home> {
+  String network = "no";
+
   @override
   Widget build(BuildContext context) {
-   return const Scaffold();
-  }
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(network, style: const TextStyle(fontSize: 40),),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              try{
+                final result = await http.get(Uri.parse('https://www.google.com'));
+                setState(() {
+                  network = "Yes";
+                });
+              } on SocketException {
+                setState(() {
+                  network = "No";
+                });
 
+              }
+            },
+            child: const Icon(Icons.refresh),
+          )
+        ],
+      ),
+    );
+  }
 }
